6f050616e9385f7ec0d99bc78319875a6cdf74dc
Abdelilah BOUROUDA <abdolilahbourouda@gmail.com>
2025-08-12 18:37:00 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/AbdelilahBOUROUDA/', // Must match your repo name exactly
+  base: '/', // Must match your repo name exactly
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
